0e469d766b3336aae71350b81ec28ddee1fa21bb
bianca2008-by <a.biancasuarez@icea.edu.ar>
2024-10-01 18:09:16 +0000

    m

--- hola.py
+++ hola.py
@@ -1,1 +1,13 @@
-hola
+numero=1
+booleano=True
+print(booleano)
+string='hola'
+print(string)
+numero=2
+print(1,booleano)
+nombreCompleto='gabriel sanchez'
+edad=23.3
+if 27<30:
+    nenoroigual=5<=9
+while 20==(10+10):
+    print(1)
